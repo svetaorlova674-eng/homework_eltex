b9de93141dd98b1097200e30c5d3fec5091f6100
Svetlana <svetaorlova674@gmail.com>
2026-05-22 22:17:34 +0700

    lesson_22 fix

--- blog-app/src/app/services/post/post-api.service.ts
+++ blog-app/src/app/services/post/post-api.service.ts
@@ -9,19 +9,19 @@ import { Comment } from './types/comment';
 export class PostApiService implements IPostService {
   private http = inject(HttpClient);
 
-  getPost(id: number): Observable<PostDetail> {
+  getPost(id: string): Observable<PostDetail> {
     return forkJoin({
       article: this.http.get<any>(`/api/articles/${id}`),
       comments: this.http.get<any[]>(`/api/comments/article/${id}`)
     }).pipe(
       map(({ article, comments }) => ({
-        id: article.id,
+        id: String(article.id),
         title: article.title,
         description: article.content,
-        image: article.imgSrc ?? 'images/post.jpg',
+        image: article.imgSrc ?? 'images/paris.png',
         rating: article.rating ?? 0,
         comments: comments.map((c: any) => ({
-          id: c.id,
+          id: String(c.id),
           author: c.username,
           text: c.content,
           rating: c.rating ?? 0,
@@ -31,21 +31,21 @@ export class PostApiService implements IPostService {
     );
   }
 
-  updatePostRating(postId: number, rating: number): Observable<PostDetail> {
+  updatePostRating(postId: string, rating: number): Observable<PostDetail> {
     return this.http.post<any>(`/api/articles/vote/${postId}`, { vote: rating }).pipe(
       switchMap(() => this.getPost(postId))
     );
   }
 
-  updateCommentRating(postId: number, commentId: number, rating: number): Observable<PostDetail> {
+  updateCommentRating(postId: string, commentId: string, rating: number): Observable<PostDetail> {
     return this.http.patch<any>(`/api/comments/${commentId}/rating`, { rating }).pipe(
       switchMap(() => this.getPost(postId))
     );
   }
 
-  addComment(postId: number, comment: Omit<Comment, 'id' | 'date'>): Observable<PostDetail> {
+  addComment(postId: string, comment: Omit<Comment, 'id' | 'date'>): Observable<PostDetail> {
     return this.http.post<any>('/api/comments', {
-      articleId: String(postId),
+      articleId: postId,
       username: comment.author,
       content: comment.text
     }).pipe(
